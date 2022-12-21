8d5a601af11ae0832585c71663a981176fa85e78
Casey Wittrock <caseywittrockmn@gmail.com>
2022-12-21 08:19:02 -0600

    rest and spread assingment

---
+++ rest_and_spread/script.js
@@ -0,0 +1,13 @@
+/*
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function(num) {
+      return num % 2 === 0
+    });
+  }
+  */
+
+  const filterOutOdds = (...nums) => nums.filter(num => num % 2 === 0);
+
+
+  const findMin = (...nums) => Math.min(...nums);
