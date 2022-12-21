0e6fed68b89a821fce9e591dff1df69467821f63
Casey Wittrock <caseywittrockmn@gmail.com>
2022-12-21 08:37:56 -0600

    finished rest spread

--- rest_and_spread/script.js
+++ rest_and_spread/script.js
@@ -1,13 +1,36 @@
-/*
-function filterOutOdds() {
-    var nums = Array.prototype.slice.call(arguments);
-    return nums.filter(function(num) {
-      return num % 2 === 0
-    });
-  }
-  */
+const filterOutOdds = (...nums) => nums.filter((num) => num % 2 === 0);
 
-  const filterOutOdds = (...nums) => nums.filter(num => num % 2 === 0);
+const findMin = (...nums) => Math.min(...nums);
 
+const mergeObjects = (objOne, objTwo) => ({ ...objOne, ...objTwo });
 
-  const findMin = (...nums) => Math.min(...nums);
+const doubleAndReturnArgs = (arr, ...args) => [
+  ...arr,
+  ...args.map((num) => num * 2),
+];
+
+const removeRandom = (items) => {
+  let indx = Math.floor(Math.random() * items.length);
+  return [...items.slice(0, indx), ...items.slice(indx + 1)];
+};
+
+const extend = (arrayOne, arrayTwo) => {
+  return [...arrayOne, ...arrayTwo];
+};
+
+const addKeyVal = (obj, key, value) => {
+  return { ...obj, [key]: value };
+};
+
+const removeKey = (obj, key) => {
+  ({ [key]: undefined, ...obj } = obj);
+  return obj;
+};
+
+const combine = (obj1, obj2) => {
+  return { ...obj1, ...obj2 };
+};
+
+const update = (obj, key, val) => {
+  return { ...obj, [key]: val };
+};
