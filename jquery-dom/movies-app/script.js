054b4c59731ea6cf0762079821d5b829572f1628
Casey Wittrock <caseywittrockmm@gmail.com>
2023-01-22 05:10:19 -0600

    added css file, movie class to jquery assignment.

--- jquery-dom/movies-app/script.js
+++ jquery-dom/movies-app/script.js
@@ -5,10 +5,33 @@ const removeMovie = (target) => {
 $("#submit").on("click", (event) => {
   event.preventDefault();
   const movieTitle = $("#title").val();
-  const rating = $('#rating').val();
-  
+  const rating = $("#rating").val();
+  new Movie();
 });
 
 $("#movie-list").on("click", "button", (evt) => {
   removeMovie(evt.target);
 });
+
+class Movie {
+  constructor() {
+    this.title = this.getTitle();
+    this.rating = this.getRating();
+    this.placeInList();
+    this.placeDeleteButton();
+  }
+  getTitle() {
+    console.log("getting title");
+    return $("#title").val();
+  }
+  getRating() {
+    console.log("getting rating");
+    return $("#rating").val();
+  }
+  deleteMovie(event) {}
+  placeInList() {
+    const movieTitle = $("<td>").text(`${this.title}`);
+    const movieRating = $("<td>").text(`${this.rating}`);
+    const 
+  }
+}
