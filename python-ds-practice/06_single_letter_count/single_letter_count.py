dc282d21e6cf5f4c77f9f23f2d28eb948b5dfbfb
Casey Wittrock <caseywittrockmn@gmail.com>
2023-03-05 01:05:50 -0600

    finish single_letter_count and is_palindrome exercises

--- python-ds-practice/06_single_letter_count/single_letter_count.py
+++ python-ds-practice/06_single_letter_count/single_letter_count.py
@@ -1,14 +1,18 @@
-def single_letter_count(word, letter):
-    """How many times does letter appear in word (case-insensitively)?
-    
-        >>> single_letter_count('Hello World', 'h')
-        1
-        
-        >>> single_letter_count('Hello World', 'z')
-        0
-        
-        >>> single_letter_count("Hello World", 'l')
-        3
+def single_letter_count(string, letter):
+    """How many times doesrun letter appear in word (case-insensitively)?
+
+    >>> single_letter_count('Hello World', 'h')
+    1
+
+    >>> single_letter_count('Hello World', 'z')
+    0
+
+    >>> single_letter_count("Hello World", 'l')
+    3
     """
 
-    for letter in word
+    count = 0
+    for i in string.lower():
+        if i == letter.lower():
+            count += 1
+    return count
