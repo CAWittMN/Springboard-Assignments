8a314abcdaf7e324b3b164a954fb406a55c825ff
Casey Wittrock <caseywittrockmn@gmail.com>
2023-03-05 13:09:10 -0600

    finish capatalize and recode of single letter count

--- python-ds-practice/06_single_letter_count/single_letter_count.py
+++ python-ds-practice/06_single_letter_count/single_letter_count.py
@@ -1,4 +1,4 @@
-def single_letter_count(string, letter):
+def single_letter_count(phrase, letter):
     """How many times doesrun letter appear in word (case-insensitively)?
 
     >>> single_letter_count('Hello World', 'h')
@@ -10,9 +10,4 @@ def single_letter_count(string, letter):
     >>> single_letter_count("Hello World", 'l')
     3
     """
-
-    count = 0
-    for i in string.lower():
-        if i == letter.lower():
-            count += 1
-    return count
+    return phrase.lower().count(letter.lower())
