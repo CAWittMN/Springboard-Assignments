3a920bff9c09ee4c9a8e740cb64ceaa8b46409d3
Casey Wittrock <caseywittrockmn@gmail.com>
2023-03-05 23:24:59 -0600

    finish more sections

--- python-ds-practice/11_flip_case/flip_case.py
+++ python-ds-practice/11_flip_case/flip_case.py
@@ -11,3 +11,9 @@ def flip_case(phrase, to_swap):
     'AaaaHHH'
 
     """
+    swapped = ""
+    for letter in phrase:
+        if letter.lower() == to_swap.lower():
+            letter = letter.swapcase()
+        swapped += letter
+    return swapped
