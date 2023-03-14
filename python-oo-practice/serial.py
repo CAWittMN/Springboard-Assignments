26f335b364e3692650c7878da033a6709af221d7
Casey Wittrock <caseywittrockmn@gmail.com>
2023-03-14 15:58:34 -0500

    finish serial.py section

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -1,8 +1,16 @@
 """Python serial number generator."""
 
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
+    Attributes
+    -----------
+    start_point: int
+        the provided starting point for the generator that can be reset to
+    curr_serial: int
+        the current serial that will be generated
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -20,3 +28,22 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0) -> None:
+        """Make a new generator instance starting at provided start"""
+        self.start_point = start
+        self.curr_serial = start
+
+    def __repr__(self) -> str:
+        """print representation"""
+        return f"""SerialGenerator
+        start_point = {self.start_point} 
+        curr_serial = {self.curr_serial}"""
+
+    def generate(self):
+        """Generate the next serial"""
+        self.curr_serial += 1
+        return self.curr_serial - 1
+
+    def reset(self):
+        """Reset the generator to the original start"""
+        self.curr_serial = self.start_point
