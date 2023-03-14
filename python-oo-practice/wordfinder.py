ca9ce30c500e6582c60b623e8cb9104481cdb1e6
Casey Wittrock <caseywittrockmn@gmail.com>
2023-03-14 17:05:35 -0500

    finish wordfinder.py section

--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -1,5 +1,49 @@
 """Word Finder: finds random words from a dictionary."""
+import random
 
 
 class WordFinder:
-    ...
+    """Machine for getting random words from a file
+    >>> finder = WordFinder("test.txt")
+    5 words read
+
+    Attributes:
+    ------------
+    words: lst of str
+        the list of words generated from the provided file
+
+    >>> finder.random() in ["One", "Two", "Three", "Four", "Five"]
+    True
+
+    >>> finder.random() in ["One", "Two", "Three", "Four", "Five"]
+    True
+
+    >>> finder.random() in ["One", "Two", "Three", "Four", "Five"]
+    True
+
+    >>> finder.random() in ["One", "Two", "Three", "Four", "Five"]
+    True
+
+    >>> finder.random() in ["One", "Two", "Three", "Four", "Five"]
+    True
+    """
+
+    def __init__(self, file_path) -> None:
+        """Read the file, call to make the words list, tells num of words in the list"""
+        word_file = open(file_path)
+        self.words = self.make_word_list(word_file)
+        print(f"{len(self.words)} words read")
+
+    def __repr__(self) -> str:
+        """represents the number of words currently usable"""
+        return f"""WordFinder
+        number of words = {len(self.words)}
+        """
+
+    def make_word_list(self, file):
+        """create a list of every word from the provided file"""
+        return [word.strip() for word in file]
+
+    def random(self):
+        """get a random word from the list"""
+        return random.choice(self.words)
