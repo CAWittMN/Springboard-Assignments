dc638d491f7dd3da87d7c2702dacb5b6475915d3
Casey Wittrock <caseywittrockmn@gmail.com>
2023-04-26 01:53:20 -0500

    finish normalization for music schema

--- sql-ddl-design/music.sql
+++ sql-ddl-design/music.sql
@@ -7,27 +7,164 @@ CREATE DATABASE music;
 
 \c music
 
+CREATE TABLE albums
+(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  release_date DATE NOT NULL
+);
+
+CREATE TABLE producers
+(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL
+);
+
+CREATE TABLE artists
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  genre TEXT NOT NULL
+);
+
 CREATE TABLE songs
 (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
-  release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  album_id INTEGER NOT NULL REFERENCES albums (id)
 );
 
-INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+
+CREATE TABLE songs_artists
+(
+  song_id INTEGER NOT NULL REFERENCES songs (id) ON DELETE CASCADE,
+  artist_id INTEGER NOT NULL REFERENCES artists (id) ON DELETE CASCADE
+);
+
+
+
+CREATE TABLE songs_producers
+(
+  song_id INTEGER NOT NULL REFERENCES songs (id),
+  producer_id INTEGER NOT NULL REFERENCES producers (id)
+);
+
+
+INSERT INTO albums
+(title, release_date)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+('Middle of Nowhere', '04-15-1997'),
+('A Night at the Opera', '10-31-1975'),
+('Daydream', '11-14-1995'),
+('A Star Is Born', '09-27-2018'),
+('Silver Side Up', '08-21-2001'),
+('The Blueprint 3', '10-20-2009'),
+('Prism', '12-17-2013'),
+('Hands All Over', '06-21-2011'),
+('Let Go', '05-14-2002'),
+('The Writing''s on the Wall', '11-07-1999');
+
+INSERT INTO producers
+  (title)
+VALUES
+  ('Dust Brothers'),
+  ('Stephen Lironi'),
+  ('Roy Thomas Baker'),
+  ('Walter Afanasieff'), 
+  ('Benjamin Rice'),
+  ('Rick Parashar'),
+  ('Al Shux'),
+  ('Max Martin'),
+  ('Cirkut'),
+  ('Shellback'),
+  ('Benny Blanco'),
+  ('The Matrix'),
+  ('Darkchild');
+
+INSERT INTO songs
+  (title, duration_in_seconds, album_id)
+VALUES
+  ('MMMBop', 238, 1),
+  ('Bohemian Rhapsody', 355, 2),
+  ('One Sweet Day', 282, 3),
+  ('Shallow', 216, 4),
+  ('How You Remind Me', 223, 5),
+  ('New York State of Mind', 276, 6),
+  ('Dark Horse', 215, 7),
+  ('Moves Like Jagger', 201, 8),
+  ('Complicated', 244, 9),
+  ('Say My Name', 240, 10);
+
+INSERT INTO artists
+  (name, genre)
+VALUES
+  ('Hanson', 'Pop Rock'),
+  ('Queen', 'Progressive Rock'),
+  ('Mariah Cary', 'Pop'),
+  ('Boyz II Men', 'R&B'),
+  ('Lady Gaga', 'Pop'),
+  ('Bradley Cooper', 'Folk Pop'),
+  ('Nickelback', 'Post Grunge'),
+  ('Jay Z', 'Hip-hop'),
+  ('Alicia Keys', 'Pop'),
+  ('Katy Perry', 'Pop'),
+  ('Juicy J', 'Hip-hop'),
+  ('Maroon 5', 'Pop'),
+  ('Christina Aguilera', 'Pop'),
+  ('Avril Lavigne', 'Pop Punk'),
+  ('Destiny''s Child', 'R&B');
+
+  INSERT INTO songs_artists
+    (song_id, artist_id)
+  VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (3, 4),
+    (4, 5),
+    (4, 6),
+    (5, 7),
+    (6, 8),
+    (6, 9),
+    (7, 10),
+    (7, 11),
+    (8, 12),
+    (8, 13),
+    (9, 14),
+    (10, 15);
+
+
+
+
+INSERT INTO songs_producers
+  (song_id, producer_id)
+VALUES
+  (1, 1),
+  (1, 2),
+  (2, 3),
+  (3, 4),
+  (4, 5),
+  (5, 6),
+  (6, 7),
+  (7, 8),
+  (7, 9),
+  (8, 10),
+  (8, 11),
+  (9, 12),
+  (10, 13);
+  
+
+
+
+
+
+
+
+
+
+ SELECT s.title, duration_in_seconds as duration, a.title as album, ar.name as artist 
+ FROM songs s 
+ FULL JOIN albums a ON s.album_id = a.id 
+ FULL JOIN songs_artists sa  ON s.id = sa.song_id 
+ FULL JOIN artists ar ON sa.artist_id = ar.id;
